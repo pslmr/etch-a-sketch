d7573b780d8956ecbe6900f71d97836237d50710
Psalmer Abalon <abalonpsalmer@gmail.com>
2022-08-04 02:06:33 +0800

    Add colorpicker function in myscript.js

--- myscript.js
+++ myscript.js
@@ -1,63 +1,8 @@
-// const sketchPad = document.querySelector(".sketchPad");
-// const setSizeBtn = document.querySelector(".size-btn");
-// const eraseBtn = document.querySelector(".erase-btn");
-
-// function initGrid() {
-// 	for (let i = 1; i <= 16 * 16; i++) {
-// 		const gridBox = document.createElement("div");
-// 		gridBox.setAttribute("class", "gridBox");
-// 		sketchPad.appendChild(gridBox);
-// 		gridBox.addEventListener("mouseover", (e) => {
-// 			gridBox.style.backgroundColor = "black";
-// 		});
-// 	}
-// }
-
-// function newGrid(size) {
-// 	while (sketchPad.firstChild) {
-// 		sketchPad.removeChild(sketchPad.firstChild);
-// 	}
-
-// 	for (let i = 1; i <= size * size; i++) {
-// 		const gridBox = document.createElement("div");
-// 		gridBox.setAttribute("class", "gridBox");
-// 		sketchPad.appendChild(gridBox);
-// 		gridBox.addEventListener("mouseover", () => {
-// 			gridBox.style.backgroundColor = "black";
-// 		});
-// 		sketchPad.setAttribute("style", `grid-template-columns: repeat(${size}, 1fr); grid-template-rows: repeat(${size}, 1fr);`);
-// 	}
-// }
-
-// setSizeBtn.addEventListener("click", () => {
-// 	let size = prompt("Enter desired grid size per side, must be not greater than 100: ", "16");
-// 	newGrid(validateSize(size));
-// });
-
-// function validateSize(size) {
-// 	if (size > 100) {
-// 		while (true) {
-// 			size = prompt("Please enter grid size per side, must be not greater than 100: ", "16");
-// 			if (size > 100) {
-// 				continue;
-// 			} else {
-// 				return size;
-// 			}
-// 		}
-// 	}
-// 	return size;
-// }
-
-// eraseBtn.addEventListener('click', () => {
-
-// });
-
-// initGrid();
-
 const canvas = document.querySelector(".canvas");
 const sliderValue = document.querySelector(".slider-value");
 const slider = document.querySelector("#slider");
 const cells = canvas.children;
+const color = document.querySelector("#colorpicker");
 
 function initGrid() {
 	const sliderValue = document.getElementById("slider").value;
@@ -71,8 +16,6 @@ function initGrid() {
 		});
 	}
 }
-
-initGrid();
 
 const clearBtn = document.querySelector(".new-btn");
 
@@ -122,3 +65,21 @@ gridLineBtn.addEventListener("click", () => {
 		cells[i].classList.toggle("cell");
 	}
 });
+
+const grayscaleBtn = document.querySelector(".grayscale-btn");
+grayscaleBtn.addEventListener("mouseover", () => {
+	let value = document.getElementById("slider").value;
+});
+
+color.addEventListener("change", () => {
+	let colorVal = document.getElementById("colorpicker").value;
+	let value = document.getElementById("slider").value;
+	for (let i = 0; i < value * value; i++) {
+		cells[i].addEventListener("mouseover", (e) => {
+			e.target.style.backgroundColor = colorVal;
+		});
+	}
+});
+
+
+initGrid();
